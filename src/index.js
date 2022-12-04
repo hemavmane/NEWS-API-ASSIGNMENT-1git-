00d8d33ac07f2cb7949326c6da2e2c4b9a8f7586
hemavmane <hemamane5376@gmail.com>
2022-12-04 11:50:23 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import {Search} from './Search/Seach'
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
@@ -11,6 +11,7 @@ import { FooterProvider } from "./Component/JS-File/Footer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* <Search/> */}
     <BrowserRouter>
      
         <ChangeModeProvider>
@@ -20,7 +21,7 @@ root.render(
         </ChangeModeProvider>
         <FooterProvider>
         </FooterProvider>
-    </BrowserRouter>
+    // </BrowserRouter>
   </React.StrictMode>
 );
 
